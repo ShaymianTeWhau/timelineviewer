d647fbc58782013904c7361b4c8e6590511cacab
ShaymianTeWhau <shaytewhau@gmail.com>
2025-02-20 12:04:27 +1300

    add setters for timeline focusDate and scaleType

--- timelineViewer.js
+++ timelineViewer.js
@@ -1,5 +1,34 @@
 class Timeline {
+  #focusDate = new Date();
+  #scaleType = "year";
+  #scaleWidth = 100; // in pixels
   constructor() {}
+  setFocusDate(focusDate) {
+    this.#focusDate = focusDate;
+  }
+  setScaleType(scaleType) {
+    const validScaleTypes = [
+      "millennium",
+      "century",
+      "decade",
+      "year",
+      "month",
+      "day",
+      "hour",
+      "second",
+      "millisecond",
+    ];
+    if (!validScaleTypes.includes(scaleType)) {
+      throw new Error(
+        "Invalid scaleType, scaleType cannot be '" + scaleType + "' must be " + validScaleTypes
+      );
+    }
+    this.#scaleType = scaleType;
+  }
+  draw(ctx, canvas, focusDate, scaleType, scaleWidth) {
+    this.setFocusDate(focusDate);
+    this.setScaleType(scaleType);
+  }
 }
 
 function setupCanvas() {
@@ -16,10 +45,16 @@ function setupCanvas() {
   }
 
   const ctx = canvas.getContext("2d");
-  const timeline = new Timeline();
 
   canvas.width = window.innerWidth;
   canvas.height = 1000;
+
+  const timeline = new Timeline();
+  let focusDate = new Date(2018, 11, 24, 10, 33, 30, 12);
+  let scaleType = "years";
+  let scaleWidth = 100;
+  timeline.draw(ctx, canvas, focusDate, scaleType, scaleWidth);
+  console.log("focus date: " + focusDate + " ms:" + focusDate.getMilliseconds());
 }
 
 window.addEventListener("load", setupCanvas);

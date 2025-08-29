aebd8a78c13aef85e96cbb9e055dc5623a7f0e27
ShaymianTeWhau <shaytewhau@gmail.com>
2025-08-29 21:39:48 +1200

    add link on index page to timeline page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
+import TimelinePage from "./timeline/page";
 
 export default function Home() {
   return (
-    <main>Default Page</main>
+    <main>
+      <Link href="/timeline">Click here to see timelineviewer</Link>
+    </main>
   );
 }
